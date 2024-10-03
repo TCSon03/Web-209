2f805c34e507c3363388385e6c5c32ae07267eab
TCSon03 <truongcongsson.@gmail.com>
2024-10-03 18:34:35 +0700

    new

--- MVC-MOGO/models/ProductModel.js
+++ MVC-MOGO/models/ProductModel.js
@@ -2,12 +2,18 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const productSchema = new Schema({
-  //khai bao cac truong va kieu du lieu o trong bang product
-  title: { type: String },
-  price: { type: Number },
-  image: { type: String },
-  description: { type: String },
-});
+const productSchema = new Schema(
+  {
+    //khai bao cac truong va kieu du lieu o trong bang product
+    title: { type: String, require: true },
+    price: { type: Number, require: true },
+    image: { type: String, require: true },
+    description: { type: String, require: true },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Product", productSchema);
